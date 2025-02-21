00e23066d69c33a9f2a4cbcae81e0dba2e935ac5
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-21 05:00:25 +0300

    Refactor Home component to include NoiseBackground and Week components

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, ImageBackground, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import NoiseBackground from '@/components/partials/noise-background';
+import Week from '@/components/partials/week';
+import { Accordion } from '@animatereactnative/accordion';
 
 export default function Home() {
   return (
-    <SafeAreaView className='flex-1 justify-center items-center'>
-      <Text className='text-center text-2xl text-selected font-barlow-700'>
-        Home
-      </Text>
+    <SafeAreaView className='flex-1'>
+      <NoiseBackground />
+      <Week />
     </SafeAreaView>
   );
 }
