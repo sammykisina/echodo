6dc00a7a2629aff87f54dbb95cecfce8508443fa
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-21 05:38:52 +0300

    Wrap Week component in ScrollView for improved scrolling behavior

--- components/partials/week.tsx
+++ components/partials/week.tsx
@@ -1,14 +1,16 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import React from 'react';
 import { weekDays } from '@/utils/constants';
 import Day from './day';
 
 export default function Week() {
   return (
-    <View>
-      {weekDays?.map((day) => (
-        <Day day={day} key={`day-${day}`} />
-      ))}
-    </View>
+    <ScrollView>
+      <View>
+        {weekDays?.map((day) => (
+          <Day day={day} key={`day-${day}`} />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
