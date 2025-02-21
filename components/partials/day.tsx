da9b742a39fe4ea0b8bbb021351f35a131ec95a6
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-21 05:38:43 +0300

    Refactor Day component to use Accordion for better UI interaction and adjust layout based on window dimensions

--- components/partials/day.tsx
+++ components/partials/day.tsx
@@ -1,27 +1,35 @@
-import { View, Text } from 'react-native';
+import { View, Text, useWindowDimensions } from 'react-native';
 import React from 'react';
 import { localFormatter, weekDayFormatter, weekDays } from '@/utils/constants';
 import dayjs from 'dayjs';
 import Todos from './todos';
+import Accordion from '@animatereactnative/accordion';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Day({ day }: { day: string }) {
+  const { height } = useWindowDimensions();
+  const { top, bottom } = useSafeAreaInsets();
+
   return (
-    <View
+    <Accordion.Accordion
+      isOpen={dayjs(day).isSame(dayjs(), 'day')}
       style={{
-        minHeight: `${100 / weekDays.length}%`,
+        minHeight: (height - top - bottom) / weekDays.length,
       }}
-      className='h-1/5 p-4 pl-12 border-t-4 border-black/10 gap-2'
+      className='h-1/5 pt-4 pr-4 pl-12 border-t-2 border-black/10 gap-2'
     >
-      <View>
-        <Text className='text-2xl uppercase font-barlow-900'>
+      <Accordion.Header>
+        <Text className='text-4xl uppercase font-barlow-900'>
           {dayjs(day).format(weekDayFormatter)}
         </Text>
         <Text className='font-barlow-400 text-gray-600'>
           {dayjs(day).format(localFormatter)} - 14°C
         </Text>
-      </View>
+      </Accordion.Header>
 
-      <Todos day={day} />
-    </View>
+      <Accordion.Expanded>
+        <Todos day={day} />
+      </Accordion.Expanded>
+    </Accordion.Accordion>
   );
 }
