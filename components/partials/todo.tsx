8bd38728ba8929cc1b84f777845c177a974cfa15
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 04:13:35 +0300

    Enhance Todo component with Check icon for completed tasks and adjust styling

--- components/partials/todo.tsx
+++ components/partials/todo.tsx
@@ -1,17 +1,22 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import type { Todo } from '@/utils/mock';
+import { Check } from 'lucide-react-native';
 
 export default function Todo({ todo }: { todo: Todo }) {
   return (
-    <View className='flex-row  items-center gap-2'>
+    <View className='flex-row gap-2 items-center pl-12'>
       <View
-        className={`w-4 h-4 rounded-sm bg-selected border  ${
+        className={`w-4 h-4 rounded-sm border items-center justify-center  ${
           todo?.done
             ? 'bg-selected border-selected'
             : 'bg-transparent border-black'
         }`}
-      ></View>
+      >
+        {todo?.done && (
+          <Check absoluteStrokeWidth size={10} className=' stroke-white  ' />
+        )}
+      </View>
 
       <Text
         className={`font-barlow-400 ${
