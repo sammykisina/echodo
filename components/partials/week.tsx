a3026b91ee866c18b560201f3e347cbf2c72f0a5
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 17:36:19 +0300

    Remove unused imports and commented code for clarity in Week component

--- components/partials/week.tsx
+++ components/partials/week.tsx
@@ -1,21 +1,18 @@
-import { ScrollView, View } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
 import { weekDays } from '@/utils/constants';
 import Day from './day';
-import { useAnimatedKeyboard } from 'react-native-reanimated';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 
 export default function Week() {
   /**
    * === STATES ===
    */
-  const { height } = useAnimatedKeyboard();
 
   return (
     <KeyboardAwareScrollView
       bottomOffset={62}
       keyboardShouldPersistTaps='handled'
-      // disableScrollOnKeyboardHide={true}
     >
       <View>
         {weekDays?.map((day) => (
