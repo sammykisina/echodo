46e45c5d743db8d53d69aab510a4b0ca9a5c0e75
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 19:12:04 +0300

    Add safe area insets to Week component for improved layout on devices

--- components/partials/week.tsx
+++ components/partials/week.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import { weekDays } from '@/utils/constants';
 import Day from './day';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Week() {
   /**
    * === STATES ===
    */
+  const { bottom } = useSafeAreaInsets();
 
   return (
     <KeyboardAwareScrollView
       bottomOffset={62}
       keyboardShouldPersistTaps='handled'
     >
-      <View>
+      <View style={{ marginBottom: bottom }}>
         {weekDays?.map((day) => (
           <Day day={day} key={`day-${day}`} />
         ))}
