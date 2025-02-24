1d19aac7231c5a91ee2f4047da6b21a717fa5ada
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 19:12:15 +0300

    Add weekDayColors constant for improved styling of weekdays

--- utils/constants.ts
+++ utils/constants.ts
@@ -11,3 +11,14 @@ export const weekDayFormatter = 'dddd';
 export const weekDays = [...Array(7).keys()].map((key) =>
   dayjs().startOf('isoWeek').add(key, 'day').format(globalFormatter)
 );
+
+const _opacity = 0.5;
+export const weekDayColors = {
+  1: `rgba(215, 215, 215,${_opacity})`,
+  2: `rgba(206, 206, 206,${_opacity})`,
+  3: `rgba(201, 201, 201,${_opacity})`,
+  4: `rgba(192, 192, 192,${_opacity})`,
+  5: `rgba(172, 172, 172,${_opacity})`,
+  6: `rgba(172, 172, 172,${_opacity})`,
+  7: `rgba(172, 172, 172,${_opacity})`,
+};
