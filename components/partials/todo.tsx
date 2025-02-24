07024bdea041fd9edc933b766378d866f09a26f9
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 17:36:12 +0300

    Add touch handling to update todo completion status in Todo component

--- components/partials/todo.tsx
+++ components/partials/todo.tsx
@@ -3,10 +3,20 @@ import React from 'react';
 import type { Todo } from '@/utils/mock';
 import { Check } from 'lucide-react-native';
 import { todos } from '@/db/schema';
+import { db } from '@/db/init';
+import { eq } from 'drizzle-orm';
 
 export default function Todo({ todo }: { todo: typeof todos.$inferSelect }) {
   return (
-    <View className='flex-row gap-2 items-center'>
+    <View
+      className='flex-row gap-2 items-center'
+      onTouchStart={() => {
+        db.update(todos)
+          .set({ done: Boolean(todo.done) ? 0 : 1 })
+          .where(eq(todos.id, todo.id))
+          .execute();
+      }}
+    >
       <View
         className={`w-4 h-4 rounded-sm border items-center justify-center  ${
           Boolean(todo?.done)
