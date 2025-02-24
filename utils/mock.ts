e5b0ca2fa737895bf6bbf5f70952c46ff4fe1e9c
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 04:13:58 +0300

    Refactor Todo type definition for improved clarity and maintainability

--- utils/mock.ts
+++ utils/mock.ts
@@ -4,11 +4,10 @@ export const _todos = [
   { content: 'Read a book', done: false, id: 3 },
   { content: 'Go for a walk', done: false, id: 4 },
   { content: 'Watch a movie', done: false, id: 5 },
-  { content: 'Cook dinner', done: false, id: 6 },
-  { content: 'Do laundry', done: false, id: 7 },
-  { content: 'Call mom', done: false, id: 8 },
-  { content: 'Take a nap', done: false, id: 9 },
-  { content: 'Clean the house', done: false, id: 10 },
-] as const;
+];
 
-export type Todo = (typeof _todos)[number];
+export type Todo = {
+  content: string;
+  done: boolean;
+  id: number;
+};
