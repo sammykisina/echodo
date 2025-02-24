6c7f69df2c45cf83fc93a76551723ed09842f9e5
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 04:13:53 +0300

    Integrate lucide-react-native icons into cssInterop for enhanced styling support

--- utils/cssInterop.ts
+++ utils/cssInterop.ts
@@ -1,6 +1,14 @@
 import { Stagger } from '@animatereactnative/stagger';
 import { LinearGradient } from 'expo-linear-gradient';
-import { cssInterop, remapProps } from 'nativewind';
+import { icons } from 'lucide-react-native';
+import { cssInterop } from 'nativewind';
+
+Object.keys(icons).forEach((key) => {
+  const IconComponent = icons[key];
+  cssInterop(IconComponent, {
+    className: 'style',
+  });
+});
 
 cssInterop(LinearGradient, {
   className: 'style',
