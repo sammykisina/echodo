a97c265cb91cb95d36583c38f831190f6ec2c8b5
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 18:20:48 +0300

    Add animated transitions for todo completion status in Todo component

--- components/partials/todo.tsx
+++ components/partials/todo.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import type { Todo } from '@/utils/mock';
 import { Check } from 'lucide-react-native';
 import { todos } from '@/db/schema';
 import { db } from '@/db/init';
 import { eq } from 'drizzle-orm';
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated';
 
 export default function Todo({ todo }: { todo: typeof todos.$inferSelect }) {
   return (
@@ -18,20 +18,25 @@ export default function Todo({ todo }: { todo: typeof todos.$inferSelect }) {
       }}
     >
       <View
-        className={`w-4 h-4 rounded-sm border items-center justify-center  ${
+        className={`w-4 h-4 rounded-sm border items-center justify-center  transition-all duration-300  ${
           Boolean(todo?.done)
             ? 'bg-selected border-selected'
             : 'bg-transparent border-black'
         }`}
       >
         {Boolean(todo?.done) && (
-          <Check absoluteStrokeWidth size={10} className=' stroke-white  ' />
+          <Animated.View
+            entering={ZoomIn.springify().duration(200)}
+            exiting={ZoomOut.springify().duration(200)}
+          >
+            <Check absoluteStrokeWidth size={10} className=' stroke-white' />
+          </Animated.View>
         )}
       </View>
 
       <Text
-        className={`font-barlow-400 capitalize ${
-          Boolean(todo?.done) ? 'line-through decoration-selected ' : ''
+        className={`font-barlow-400 flex-shrink ${
+          Boolean(todo?.done) ? 'line-through decoration-selected' : ''
         }`}
       >
         {todo.content}
