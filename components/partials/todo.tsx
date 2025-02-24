0a90bc278e2d6dc2f02dbacb02edcb9242f2befb
sammy kisina <sammy.k.mutua@gmail.com>
2025-02-24 06:12:30 +0300

    Refactor Todo component to use inferred type for todo prop and improve boolean checks

--- components/partials/todo.tsx
+++ components/partials/todo.tsx
@@ -2,25 +2,26 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import type { Todo } from '@/utils/mock';
 import { Check } from 'lucide-react-native';
+import { todos } from '@/db/schema';
 
-export default function Todo({ todo }: { todo: Todo }) {
+export default function Todo({ todo }: { todo: typeof todos.$inferSelect }) {
   return (
     <View className='flex-row gap-2 items-center pl-12'>
       <View
         className={`w-4 h-4 rounded-sm border items-center justify-center  ${
-          todo?.done
+          Boolean(todo?.done)
             ? 'bg-selected border-selected'
             : 'bg-transparent border-black'
         }`}
       >
-        {todo?.done && (
+        {Boolean(todo?.done) && (
           <Check absoluteStrokeWidth size={10} className=' stroke-white  ' />
         )}
       </View>
 
       <Text
         className={`font-barlow-400 ${
-          todo.done ? 'line-through decoration-selected ' : ''
+          Boolean(todo?.done) ? 'line-through decoration-selected ' : ''
         }`}
       >
         {todo.content}
